38227d50879536f7aa36aafb04d9ad81b2630a35
Pere <pere.solaclaver@gmail.com>
2020-02-12 13:38:21 +0000

    Correction from Pascal

--- src/11_args.py
+++ src/11_args.py
@@ -85,3 +85,4 @@ d = {
 
 # How do you have to modify the f4 call below to make this work?
 f4(**d)
+
--- src/12_scopes.py
+++ src/12_scopes.py
@@ -5,8 +5,8 @@
 x = 12
 
 def change_x():
+    global x # < correction
     x = 99
-    print(x)
 
 change_x()
 
@@ -20,8 +20,8 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y # << correction
         y = 999
-        print(y)
 
     inner()
 
